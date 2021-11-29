622db00ba6679fa3cc9b8c019761501016880ffe
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-29 19:19:06 +0100

    snack 3 completato

--- Snack-3/index.php
+++ Snack-3/index.php
@@ -1,14 +1,32 @@
 
-
 <?php
+// generare 15 numeri casuali
+// una volta verificata l'unicità pusharli negli array
+$primoArray =[];
+$secondoArray =[];
 
+
+//avrei fatto una funzione ma non ci siamo ancora arrivati
+for ($i=0; $i < 15; $i++) { 
+    $num= random_int(0, 100);
+    if(!in_array($num, $primoArray)){
+        array_push($primoArray, $num);
+    }else{
+        $i--;
+    }
+}
+for ($a=0; $a < 15; $a++) { 
+    $num= random_int(0, 100);
+    if(!in_array($num, $secondoArray)){
+        array_push($secondoArray, $num);
+    }else{
+        $a--;
+    }
+}
+/* var_dump($primoArray); */
 
 
 ?>
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,6 +37,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <ul >
+            <h1>Primo array</h1>
+            <?php 
+            for ($i=0; $i < count($primoArray); $i++) { 
+                echo "<li><h2>".$primoArray[$i]."</h2></li>";
+            }
+            ?>
+        </ul>
+        <ul >
+        <h1>Secondo array</h1>
+
+            <?php 
+            for ($i=0; $i < count($secondoArray); $i++) { 
+                echo "<li><h2>".$secondoArray[$i]."</h2></li>";
+            }
+            ?>
+        </ul>
+    </header>
 </body>
 </html>
